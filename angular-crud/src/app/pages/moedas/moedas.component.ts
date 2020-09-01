2f5d93964dec5ce04db1f732322f8dec6eaecbf5
André Santos <andreluiz.torrens@gmail.com>
2020-09-01 12:06:33 -0300

    Adicionado Select para escolher  moedas a serem consultadas

--- angular-crud/src/app/pages/moedas/moedas.component.ts
+++ angular-crud/src/app/pages/moedas/moedas.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MoedasService } from 'src/app/services/moedas.service';
-import { Coins } from 'src/app/model/coins';
-import { Order } from 'src/app/model/coins';
+import { Coins, Order } from 'src/app/model/coins';
 
 @Component({
   selector: 'app-moedas',
@@ -14,19 +13,28 @@ export class MoedasComponent implements OnInit {
 
   constructor( private moedasService: MoedasService ) { }
 
+  moedas: string[] = ["SELECIONE SUA MOEDA AQUI: ","BTC","ETH","USDC"];
+  moedaSelecionada: string;
+
   public ngOnInit() {
     this.loadPriceCoin();
     this.loadOrderbook();
   }
 
   public loadPriceCoin() {
-    this.moedasService.getCoins('BTC')
+    this.moedasService.getCoins(this.moedaSelecionada)
       .subscribe(res => this.listPrice = res)
   }
 
   public loadOrderbook() {
-    this.moedasService.getOrderbook('BTC')
+    this.moedasService.getOrderbook(this.moedaSelecionada)
       .subscribe(res => this.listOrder = res)
+  }
+
+  public selecionouMoeda(event: any){
+    this.moedaSelecionada = event.target.value;
+    this.loadPriceCoin();
+    this.loadOrderbook();
   }
 
 }
